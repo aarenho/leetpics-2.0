709529f43761d8967fb871a93a6bba62fca29974
Aaren <aarenho11@gmail.com>
2024-12-17 00:36:59 -0800

    batman & robin + overlay

--- leetpics-2.0/src/app/page.tsx
+++ leetpics-2.0/src/app/page.tsx
@@ -4,10 +4,23 @@ import Navbar from "./components/Navbar";
 
 export default function Home() {
   return (
-    <div>
+    <div className="min-h-screen flex flex-col">
       <Navbar />
+      <div className="flex-1 flex justify-center items-center">
+        <div className="flex flex-col items-center gap-40">
+        <div className="relative">
+          <Image
+            src="/batman.png"  
+            width={500}
+            height={500}
+          />
+          <div className="absolute top-0 left-0 w-1/2 h-1/2 bg-black opacity-50" />
+        </div>
+        <h1 className="text-4xl font-bold text-white">
+          Batman
+        </h1>
+        </div>
+      </div>
     </div>
-    
-
   );
 }
