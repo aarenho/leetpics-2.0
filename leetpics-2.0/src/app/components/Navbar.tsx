6f4afb042680179ac6778b15fae2a2dd18a467a2
Aaren <aarenho11@gmail.com>
2025-01-08 01:25:11 -0800

    add collections page (non-working)

--- leetpics-2.0/src/app/components/Navbar.tsx
+++ leetpics-2.0/src/app/components/Navbar.tsx
@@ -3,7 +3,7 @@
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 
-const Navbar = () => {
+const Navbar: React.FC = () => {
     const pathname = usePathname();
 
     return (
---
+++ leetpics-2.0/src/app/components/collection.tsx
@@ -0,0 +1,15 @@
+// src/app/collection.tsx
+
+import React from 'react';
+import styles from './globals.css'; // Adjust the import based on your styles
+
+const CollectionPage: React.FC = () => {
+    return (
+        <div className={styles.container}>
+            <h1 className={styles.title}>Collection</h1>
+            <p className={styles.description}>This is a blank collection page.</p>
+        </div>
+    );
+};
+
+export default CollectionPage;
